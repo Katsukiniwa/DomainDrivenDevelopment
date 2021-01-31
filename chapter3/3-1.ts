8f4ba781422f73665a0d755f61d9aaedcfaac0ca
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-01-31 15:04:20 +0900

    Entityの比較

--- chapter3/3-1.ts
+++ chapter3/3-1.ts
@@ -1,17 +1,23 @@
 class User {
+  private readonly id: UserId; // 識別子
   private name: string;
 
-  constructor(name: string) {
+  constructor(id: UserId, name: string) {
+    if (name.length < 3) {
+      throw new Error('ユーザ名は3文字以上です')
+    }
+    this.id = id;
+    this.name = name;
+  }
+
+  public changeUserName(name: string) {
     if (name.length < 3) {
       throw new Error('ユーザ名は3文字以上です')
     }
     this.name = name;
   }
 
-  public changeName(name: string) {
-    if (name.length < 3) {
-      throw new Error('ユーザ名は3文字以上です')
-    }
-    this.name = name;
+  public equals(other: User) {
+    return this.id === other.id; // 比較はid(識別子)同士で行われる
   }
 }
